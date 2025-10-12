b4e7d02cda4bc7843ba41a36fb66d4cb69ba5c41
Johnny Gabriele <jg@defibeats.com>
2025-10-12 22:25:46 +0100

    Revert "feat(desktop-ui): scaffold Vite + React skeleton and build output"
    This reverts commit 3436966cfe6d82f05196eafa1e1df0b2339bd779.

--- desktop-ui/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-function App() {
-  return (<div style={{padding:20}}>Desktop UI Skeleton</div>)
-}
-
-const el = document.getElementById('root')!
-createRoot(el).render(<App />)
